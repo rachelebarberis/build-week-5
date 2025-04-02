0579824d14d80b32b434d81f773fe3737a8979d0
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-04-02 11:25:04 +0200

    Added login

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import store from './Redux/Store/Index.js';
+import store from './Redux/Store';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
