61b89fa688dac6be0f441d71f0d1c5a3eb44ad7c
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-04-03 15:25:37 +0200

    Commit

--- src/Redux/Actions/puppiesActions.js
+++ src/Redux/Actions/puppiesActions.js
@@ -118,9 +118,11 @@ export const addPuppy = (puppyData) => {
     dispatch(addPuppyRequest());
 
     try {
-      if (!puppyData.UserId && !puppyData.MicrochipPresente) {
-        // Rimuovo UserId e NumeroMicrochip se non sono presenti nel form
+      // Rimuovo UserId e NumeroMicrochip se non sono presenti nel form
+      if (!puppyData.UserId) {
         delete puppyData.UserId;
+      }
+      if (!puppyData.MicrochipPresente) {
         delete puppyData.NumeroMicrochip;
       }
       const response = await fetch('https://localhost:7055/api/Animali', {
