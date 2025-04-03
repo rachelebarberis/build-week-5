9c9ce416b1b2061a94e34c9db8ab155b57757337
Vittorio00 <viktoryturiaci2323@gmail.com>
2025-04-03 14:12:16 +0200

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,28 +13,6 @@ import RegisterComponent from './Components/RegisterComponent';
 import HomeComponent from './Components/HomeComponent';
 import NotAuthorized from './Components/NotAuthorized';
 import RoleRoute from './Components/roleRoute';
-import AggiungiPuppy from './Components/AggiungiPuppy';
-import ModificaPuppy from './Components/ModificaPuppy';
-import PuppyDetails from './Components/PuppyDetails';
-import VisiteCreateComponent from './Components/VisiteCreateComponent';
-import VisiteEditComponent from './Components/VisiteEditComponent';
-import DeleteVisitaComponent from './Components/DeleteVisitaComponent';
-import ClientiListComponent from './Components/Clienti/ClientiListComponent';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import NavBarComponent from './Components/NavBarComponent';
-import FooterComponent from './Components/FooterComponent';
-import Puppy from './Components/Puppy';
-import VisiteComponent from './Components/VisiteComponent';
-import ElencoPuppy from './Components/ElencoPuppy';
-import RicoveriComponent from './Components/Ricoveri/RicoveriComponent';
-import Farmacia from './Components/Farmacia';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import LoginComponent from './Components/LoginComponent';
-import RegisterComponent from './Components/RegisterComponent';
-import HomeComponent from './Components/HomeComponent';
-import NotAuthorized from './Components/NotAuthorized';
-import RoleRoute from './Components/roleRoute';
 
 const App = () => {
   return (
