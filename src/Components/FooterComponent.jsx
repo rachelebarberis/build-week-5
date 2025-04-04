621afe593b8a1e2b3cbf7ad5541179a0921c4477
vintry <oennabati3@gmail.com>
2025-04-04 14:55:12 +0200

    footer fixed

--- src/Components/FooterComponent.jsx
+++ src/Components/FooterComponent.jsx
@@ -1,16 +1,16 @@
 import { Container } from "react-bootstrap";
+
 const FooterComponent = () => (
-  <Container style={{ fontFamily: "'Poppins', sans-serif" }}>
-    <footer className="  mt-5 mb-0" id="footer">
-      <span className=" m-auto p-2 d-flex justify-content-center">
-        <strong>Clinica Veterinaria - Prenditi cura dei tuoi Puppies</strong> -
-        Copyright {new Date().getFullYear()}
-      </span>{" "}
-      {/* <div className="text-center">
-        <img src="/public/images/footer1.jpg" id="imgfooter"></img>
-      </div>*/}
-    </footer>
-  </Container>
+  <footer className="mt-auto py-3" id="footer">
+    <Container style={{ fontFamily: "'Poppins', sans-serif" }}>
+      <div className="text-center">
+        <span>
+          <strong>Clinica Veterinaria - Prenditi cura dei tuoi Puppies</strong>{" "}
+          - Copyright {new Date().getFullYear()}
+        </span>
+      </div>
+    </Container>
+  </footer>
 );
 
 export default FooterComponent;
