f7117219540ec78a49070083047da20db000f71d
vintry <oennabati3@gmail.com>
2025-04-04 15:10:42 +0200

    commit1

--- src/Components/FooterComponent.jsx
+++ src/Components/FooterComponent.jsx
@@ -5,6 +5,7 @@ const FooterComponent = () => (
     <Container style={{ fontFamily: "'Poppins', sans-serif" }}>
       <div className="text-center">
         <span>
+          {/* <div className="text-center"> <img src="/public/images/footer1.jpg" id="imgfooter"></img> </div>*/}
           <strong>Clinica Veterinaria - Prenditi cura dei tuoi Puppies</strong>{" "}
           - Copyright {new Date().getFullYear()}
         </span>
